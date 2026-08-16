75d93130a7a8ed873ac5c30f16071197bdd50472
lucas1 <nahuelluca1203@gmai.com>
2026-08-16 17:25:07 -0300

    Completa diagnostico de Node.js

--- diagnostico.js
+++ diagnostico.js
@@ -1,4 +1,8 @@
-const { version } = require('node:process');
+console.log("Hola programadores");
 
-console.log(`Version: ${version}`);
-// Version: v14.8.0
+console.log("=== DIAGNÓSTICO DE NODE.JS ===");
+
+console.log("Versión de Node.js:", process.version);
+console.log("Plataforma:", process.platform);
+console.log("Argumentos recibidos:", process.argv);
+console.log("Sistema operativo:", process.env.OS);
